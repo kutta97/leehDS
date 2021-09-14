ed6515540f4fa40d9ed408711c3ddad683d29497
ParkSeongtaek <56757197+ParkSeongtaek@users.noreply.github.com>
2021-09-14 13:46:37 +0900

    10818번
    물론 자료구조를 사용하지 않는 방법이 더 효율적임을 알고 있습니다만 자료구조 스터디 취지에 맞게 작성했습니다. 단순 효율 중시 버전은 따로 올리겠습니다

---
+++ ParkSeongTeak/10818/ArrayHeader.cpp
@@ -0,0 +1,33 @@
+#include "ArrayHeader.h"
+#include<iostream>
+
+using namespace std;
+
+Array::Array (int size) : size(size) {
+	arr = new int[size];
+}
+
+void Array::SetArr() {
+	for (int i = 0; i < size; i++) {
+		cin >> arr[i];
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+	}
+}
+
+void Array::GetMax() {
+	cout << max << " ";
+}
+
+void Array::GetMin() {
+	cout << min << " ";
+}
+
+int Array::num(int num) {
+	return arr[num];
+}
+
