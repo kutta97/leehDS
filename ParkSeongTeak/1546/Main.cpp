b5c29e2757039043933fcd21f9e1ff5754c0d09f
ParkSeongtaek <56757197+ParkSeongtaek@users.noreply.github.com>
2021-09-15 19:19:45 +0900

    Add files via upload

---
+++ ParkSeongTeak/1546/Main.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+#include "ArrayHeader.h"
+using namespace std;
+
+int main() {
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int num;
+	float sum = 0.0;
+	cin >> num;
+	Array scores(num);
+	
+	scores.SetArr();
+	int MaxScore = scores.GetMax();
+	for (int i = 0; i < scores.GetSize(); i++) {
+		sum += ((float)scores.Getnum(i) / MaxScore)*100;
+	}
+	cout.precision(10);
+	cout << sum / scores.GetSize();
+
+}
