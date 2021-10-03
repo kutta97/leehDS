26b5315bc51cd870f72b86f269e25faecf3157e3
ParkSeongtaek <56757197+ParkSeongtaek@users.noreply.github.com>
2021-10-03 20:28:49 +0900

    Create queue.hpp

---
+++ ParkSeongTeak/Queue/queue.hpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include"queue.h"
+using namespace std;
+
+template<class T>
+
+
+template<class T>
+void queue<T>::push(T data) {
+
+}
+
+template<class T>
+T queue<T>::front() {
+
+}
+
+template<class T>
+T queue<T>::back() {
+
+}
+
+template<class T>
+int queue<T>::size() {
+
+}
+template<class T>
+void queue<T>::pop() {
+
+}
+template<class T>
+bool queue<T>::empty() {
+
+}
