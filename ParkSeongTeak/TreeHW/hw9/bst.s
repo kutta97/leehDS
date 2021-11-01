ad52f00fdd3f8124ca84553e48d8f012952e8abf
ParkSeongtaek <56757197+ParkSeongtaek@users.noreply.github.com>
2021-11-01 17:38:26 +0900

    Update bst.s

--- ParkSeongTeak/TreeHW/hw9/bst.s
+++ ParkSeongTeak/TreeHW/hw9/bst.s
@@ -1,18 +1,20 @@
 #pragma once
-//\linux2: / ce / a3 / cea3025 / dir7 % cat tree.h
 #ifndef TREE_H
 #define TREE_H
+
+
 #include <iostream>
 #include <queue>
 using namespace std;
 template <class K, class E>
 struct Node {
-	Node(K ky, E el, Node<K, E>* left = 0, Node<K, E>* right = 0)
-		: key(ky), element(el), leftChild(left), rightChild(right) { }
-	Node<K, E>* leftChild;
+	Node(K ky, E el, int lsize = 1, Node<K, E>* left = 0, Node<K, E>* right = 0)
+		: key(ky), element(el), leftSize(lsize), leftChild(left), rightChild(right) { }
 
 	K key;
 	E element;
+	int leftSize;
+	Node<K, E>* leftChild;
 	Node<K, E>* rightChild;
 };
 
@@ -27,7 +29,10 @@ public:
 	void Levelorder();
 	bool Get(const K&, E&);
 	void Print();
-
+	bool RankGet(int r, K& k, E& e);
+	void Delete(K& oldkey) { 
+		Delete(root, oldkey);
+	}
 
 private: // helper 함수들
 	void Visit(Node<K, E>*);
@@ -35,6 +40,7 @@ private: // helper 함수들
 	void Preorder(Node<K, E>*);
 	void Inorder(Node<K, E>*);
 	void Postorder(Node<K, E>*);
+	void Delete(Node<K, E>*&, K&);
 	Node<K, E>* root;
 };
 template <class K, class E>
@@ -112,7 +118,80 @@ void BST<K, E>::Print() { // inorder와 postorder로
 	cout << endl << "Postorder traversal : "; Postorder();
 	cout << endl;
 }
+template <class K, class E>
+bool BST<K, E>::RankGet(int r, K& k, E& e)
+{ 
+	Node<K, E>* ptr = root;
+	while (ptr) {
+		if (r < ptr -> leftSize) ptr = ptr -> leftChild;
+		else if (r > ptr -> leftSize) {
+			r -= ptr -> leftSize;
+			ptr = ptr -> rightChild;
+		}
+		else {
+			k = ptr -> key; 
+			e = ptr -> element; 
+			return true; 
+		}
+	}
+	return false;
+}
+template <class K, class E>
+void BST<K, E>::Delete(Node<K, E>*& ptr, K& oldkey)
+{
+	// ptr를 루트로 하는 트리에서 oldkey를 키로 하는 노드 삭제
+	Node<K, E>* tmpptr; Node<K, E>* tmpdaddyptr;
+	if (ptr == 0) return; // 그런 노드가 없으므로, 그냥 return
+	if (oldkey < ptr->key)  // 좌측아들이 루트인 트리에서 삭제.
+		Delete(ptr->leftChild, oldkey);
+	else if (oldkey > ptr->key) // 우측아들이 루트인 트리에서
+		Delete(ptr->rightChild, oldkey);
+	else { // ptr노드가 바로 지울 노드인 경우임
+		if (!ptr->leftChild && !ptr->rightChild) //아들이 없다면
+		{
+			delete ptr; ptr = 0; return;
+		}
+		else if (!ptr->leftChild) // 그 아들을 ptr가 가리키게
+		// 하고, 현재 ptr가 가리키는 노드를 지움
+		{
+			tmpptr = ptr; ptr = ptr->leftChild;
+			delete tmpptr; return;
+		}
+		else if (!ptr->rightChild) { 
+			tmpptr = ptr; ptr = ptr->leftChild; 
+			delete tmpptr; return;
+		}
+		else { // 두아들 있음:루트가 r루트인 우측 subtree 에서
+		// ‘제일작은 노드’찾자
+			Node<K, E>* rc = ptr->rightChild;
+			if (!rc -> leftChild) {
+				// rc가 왼쪽아들이 없으면 rc가 그 노드! 이제 rc 노드의
+				// key/element/rightChild 정보를 ptr노드로 이동하고
+				// rc노드를 지운다(즉 delete rc);
+				ptr->key = rc->key; ptr->element = rc->element;
+				ptr->rightChild = rc->rightChild;
+				delete rc; 
+				return;
+			}
+			else { // rc의 왼쪽 아들이 있으면, 그 아들의 왼쪽아들
+			// 식으로 왼쪽 아들을 끝까지 쫒아가, 가장 작은 키 갖는
+			//노드를 찾아, 그 노드의 key/element를 ptr노드로
+			// 옮기고, 그 노드의 rightChild는 그 노드의 아빠의
+			// leftChild에 저장한 다음 그 노드를 지움
+				rc->leftSize--;
+				Node<K, E>* lc = rc->leftChild;
+				while (!lc->leftChild) {
+					lc->leftSize--;
+					tmpdaddyptr = lc;
+					lc = lc->leftChild;
+				}
+				ptr->key = lc->key; ptr->element = lc->element;
+				tmpdaddyptr->leftChild = lc->rightChild;
+				delete lc; 
+				return;
+			}
+		}
+	}
+}
 
-//.....Inorder, Postorder의 helper함수를 구현하시오.
-//.....Levelorder(교재p266 참조하되 STL 큐를 이용)를 구현하시오.
 #endif
