2951a71b3d76c86949a8bb4ca61cf26ae8352136
ParkSeongtaek <56757197+ParkSeongtaek@users.noreply.github.com>
2021-11-11 19:30:51 +0900

    Create kruskal.cpp

---
+++ ParkSeongTeak/graph/13_10/kruskal.cpp
@@ -0,0 +1,38 @@
+#include <fstream>
+#include<queue> 
+#include"mstree.h" 
+
+int NNODES; // #nodes in the graph is global
+
+priority_queue< Edge, vector<Edge>, Compare > PQ; 
+
+void kruskal() {
+	Sets sets(NNODES);
+	int nedges = 0; // #edges found up to now.
+	while (nedges < NNODES - 1) {
+		if (PQ.empty()) throw "No Spanning Tree Exists.";
+		Edge e = PQ.top(); PQ.pop();
+		int v1root = sets.Find(e.v1); int v2root = sets.Find(e.v2);
+		if (v1root != v2root) { 
+			// different sets
+			sets.Union(v1root, v2root); nedges++;
+			cout << e;
+		}
+	}
+}
+
+void ReadEdges4kruskal() {
+	Edge e; 
+	while (GetEdge(NNODES, e)) PQ.push(e);
+}
+
+int main(){
+	cin >> NNODES;
+	
+	try { ReadEdges4kruskal(); kruskal(); }
+	
+	catch (char const* str)
+	{
+		cerr << "Exception: " << str << endl; return 1;
+	}
+}
