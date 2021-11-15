f3e518817c037008af26d086fd94b5fc15a873ee
kutta97 <yhjin04170@gmail.com>
2021-11-15 09:49:13 +0900

    complete kruskal and prim

---
+++ kutta97/[DS-08] MST/mstree.h
@@ -0,0 +1,29 @@
+#ifndef MSTREE_H
+#define MSTREE_H
+#include <iostream>
+using namespace std;
+
+class Sets {
+	public:
+		Sets(int);
+		void Union(int i, int j);
+		int Find(int i);
+	private:
+		int *parent;
+		int n;
+};
+
+struct Edge {
+	int v1, v2;
+	double weight;
+	bool operator==(Edge& e2);
+	bool operator!=(Edge& e2);
+};
+
+struct Compare {
+	bool operator() (Edge e1, Edge e2) { return e1.weight > e2.weight; };
+};
+
+ostream& operator<<(ostream& os, Edge& e);
+bool GetEdge(istream& is, const int numnodes, Edge& e);
+#endif
